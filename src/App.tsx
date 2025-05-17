61d04937908ee89b61e6e270c937a6d4cd78839a
Keisei Akiya <keisei.akiya@gmail.com>
2025-05-17 21:18:21 +0900

    ポケモン詳細完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,54 @@
 import { useEffect, useState } from 'react';
-import './App.css';
-import { getAllPokemon } from './utils/pokemon';
+import { getAllPokemon, getPokemon } from './utils/pokemon';
+import type { PokemonItem, PokemonResponse } from './types/pokemon';
+import Card from './components/Card/Card';
 
 function App() {
   const initialURL: string = 'https://pokeapi.co/api/v2/pokemon/';
   const [loading, setLoading] = useState<boolean>(true);
+  const [pokemonData, setPokemonData] = useState<PokemonItem[]>([]);
 
   useEffect(() => {
     const fetchPokemonData = async (): Promise<void> => {
       // すべてのポケモンデータを取得
-      const res: Promise<unknown> = await getAllPokemon(initialURL);
+      const res: PokemonResponse = await getAllPokemon(initialURL);
       // 各ポケモンの詳細なデータを取得
-      console.log(res);
+      loadPokemon(res.results);
       setLoading(false);
     };
     fetchPokemonData();
   }, []);
 
+  const loadPokemon = async (data: PokemonItem[]) => {
+    const _pokemonData: PokemonItem[] = await Promise.all(
+      data.map(pokemon => {
+        const pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      }),
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  console.log(pokemonData);
+
   return (
-    <div className="App">
+    <div
+      className="
+        min-h-screen flex flex-col justify-center items-center w-full h-h-full
+        bg-gray-950 text-white
+      "
+    >
       {loading ? (
-        <h1>Loading...</h1>
+        // ローディング中の表示
+        <h1 className="text-2xl font-bold text-white">Loading...</h1>
       ) : (
+        // ロード完了後のポケモンデータ表示
         <>
-          <h1>ポケモンデータを取得しました</h1>
+          <div className="w-full max-w-6xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
+            {pokemonData.map((pokemon: PokemonItem, i) => {
+              return <Card key={i} pokemon={pokemon} />;
+            })}
+          </div>
         </>
       )}
     </div>
--- src/utils/pokemon.ts
+++ src/utils/pokemon.ts
@@ -1,7 +1,24 @@
-export function getAllPokemon(url: string) {
+import type { PokemonItem, PokemonResponse } from '../types/pokemon';
+
+// ポケモンのデータを取得する関数
+export function getAllPokemon(url: string): Promise<PokemonResponse> {
   return new Promise((resolve, reject) => {
     fetch(url)
       .then(res => res.json())
-      .then(data => resolve(data));
+      .then(data => resolve(data))
+      .catch(err => reject(err));
+  });
+}
+
+// 各ポケモンの詳細なデータを取得する関数
+export function getPokemon(url: string): Promise<PokemonItem> {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data);
+        resolve(data);
+      })
+      .catch(err => reject(err));
   });
 }
